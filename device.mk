36485604b54dd314e189c1b240b14d3d60bee5a9
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-22 17:50:43 +0530

    avicii: squash: Introduce DeviceSettings
    - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/f55c0218951d9b0f6586bdf5041a64ea14c96460/DeviceSettings
    - Adapted to AndroidX
    - Includes all translations from pie cycle
    - Dropped DC-Dimming QS Tile
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Make HBM and DC-Dimming tile show active status
    
    AOSiP Edits:
    Apply to DC-Dimming QS Tile too
    
    Change-Id: I5737fb878060644577c6efc4b34c3570dd99e413
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: change the color mode preview image
    
    Change the color mode preview image to better illustrate the difference
    between Natural and Boosted color modes.
    
    Taken from: https://github.com/AOSiP/platform_packages_apps_Settings/commit/eb15083e1b079402c4b3bbcafd1f040a7b33dbf4
    
    Bug: 138126243
    Test: manual testing on Pixel
    Change-Id: I367b383f9f2da9b2e2d5960e51c3ec7821d4915b
    (cherry picked from commit 56574d5924727ccc95a65c088375ca4b95bd8e79)
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Hide start margin of seek bar preference
    
    Also fix layout of preference
    
    Change-Id: I5758f3f76750d2e817bee003c616384eb575e154
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: sepolicy: Address neverallow introduced by vibration slider
    
    Change-Id: Ia12c950e714d5b4b0d4fe43d3c609fd5b865d7b0
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Update display modes for OOS 10
    
    Change-Id: Iedfa24b0c4c6a8304839ff3ddf25119a6b44bf62
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Use VibrationEffect
    
    * Also update haptic feedback logic
    
    Change-Id: I722022d014d9e88ef267a5563b43fee87ef02b6b
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: remove HBM
    
    -remove qs tile
    -remove toggle
    *doesn't work anyways.
    
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>
    Change-Id: Ida25e4ea1f06c7da6fb816d40dd0a6fa39b3a655
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Remove vibrator control
    
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    
    avicii: DeviceSettings: Nuke DisplayMode
    
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -255,6 +255,10 @@ PRODUCT_PACKAGES += \
     fstab_nodata.qcom \
     ueventd.qcom.rc \
 
+# DeviceSettings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
@@ -526,7 +530,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
