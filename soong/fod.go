4a09ea43405faed45bef080564fd17a50b527ee3
DroidFreak32 <rushabshah32@gmail.com>
2020-12-11 15:43:59 +0530

    avicii: fod: Unset conflicting FOD vars

--- soong/fod.go
+++ soong/fod.go
@@ -3,18 +3,10 @@ package lito
 import (
     "android/soong/android"
     "android/soong/cc"
-    "strings"
 )
 
 func fodFlags(ctx android.BaseContext) []string {
     var cflags []string
-
-    var config = ctx.AConfig().VendorConfig("ONEPLUS_LITO_FOD")
-    var posX = strings.TrimSpace(config.String("POS_X"))
-    var posY = strings.TrimSpace(config.String("POS_Y"))
-    var size = strings.TrimSpace(config.String("SIZE"))
-
-    cflags = append(cflags, "-DFOD_POS_X=" + posX, "-DFOD_POS_Y=" + posY, "-DFOD_SIZE=" + size)
     return cflags
 }
 
